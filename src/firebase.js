060ec465244d8de096425198172d3d10fe57d365
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-10-03 20:35:42 +0530

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAe8UiAfKMIf_2Y6YwOJ_6qK3JSWLkIfl4",
+    authDomain: "whatsapp-clone-837b0.firebaseapp.com",
+    projectId: "whatsapp-clone-837b0",
+    storageBucket: "whatsapp-clone-837b0.appspot.com",
+    messagingSenderId: "565509027465",
+    appId: "1:565509027465:web:6a3307ead2ffadfd3eb858",
+    measurementId: "G-0V8KNJ4CVF",
+};
