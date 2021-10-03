e1721a0287195297e6d6758df62689230eaecdbd
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-10-03 22:16:15 +0530

    Cleanup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+    return (
+        // BEM naming convention
+        <div className="App">
+            <h1>Let's build whatsapp Clone</h1>
+            <div className="app__body">
+                {/* sidebar */}
+                {/* chats */}
+            </div>
+        </div>
+    );
+}
+
+export default App;
